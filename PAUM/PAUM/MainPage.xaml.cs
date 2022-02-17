91c66a74f171ad9cc3a62943c4039159d04ca224
Tomasz Kaczmarzyk <tspamm3r@gmail.com>
2022-02-17 11:18:54 +0100

    Add project files.

---
+++ PAUM/PAUM/MainPage.xaml.cs
@@ -0,0 +1,111 @@
+﻿using PAUM.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace PAUM
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.ListDiff.ItemsSource = this.ListDiffLevels;
+            this.ListDiff.SelectedItem = this.NumberOfDiff;
+            this.ListAudio.ItemsSource = this.ListAudioFiles;
+            this.ListAudio.SelectionMode = ListViewSelectionMode.None;
+            this.ListInterval.ItemsSource = this.ListIntervalValues;
+            this.ListInterval.SelectedItem = this.IntervalValue;
+            this.ListLive.ItemsSource = this.ListOfLive;
+            this.ListLive.SelectedItem = this.NumberOfLive;
+            this.ListCorrect.ItemsSource = this.ListOfCorrectPasses;
+            this.ListCorrect.SelectedItem = this.NumberOfMaxCorrectPasses;
+            this.ListToThink.ItemsSource = this.ListOfTimeToThink;
+            this.ListToThink.SelectedItem = this.TimeToThink;
+        }
+
+        public int TimeToThink { get; set; } = 7;
+        public int IntervalValue { get; set; } = 5;
+        public int NumberOfLive { get; set; } = 3;
+        public int NumberOfMaxCorrectPasses { get; set; } = 5;
+        public int NumberOfDiff { get; set; } = 1;
+
+        public List<int> ListOfTimeToThink { get; set; } = new List<int>() { 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+        public List<int> ListOfCorrectPasses { get; set; } = new List<int>() { 3, 4, 5, 6, 7, 8, 9, 10 };
+        public List<int> ListOfLive { get; set; } = new List<int>() { 1, 2, 3, 4, 5 };
+        public List<int> ListDiffLevels { get; set; } = new List<int>() { 1, 2, 3 };
+        public List<int> ListIntervalValues { get; set; } = new List<int>() { 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+        public ObservableCollection<string> ListAudioFiles { get; set; } = new ObservableCollection<string>() { "1.mp3", "2.mp3", "3.mp3", "4.mp3", "5.mp3", "6.mp3" };
+
+        async void ButtonClickedAudio(object sender, EventArgs e)
+        {
+            try
+            {
+                var customFileType =
+                    new FilePickerFileType(new Dictionary<DevicePlatform, IEnumerable<string>>
+                    {
+                        { DevicePlatform.Android, new[] { "audio/*" } },
+                        { DevicePlatform.UWP, new[] { "mp3" } }
+                    });
+
+                var options = new PickOptions
+                {
+                    PickerTitle = "Please select a audio files",
+                    FileTypes = customFileType
+                };
+                var result = await FilePicker.PickMultipleAsync(options);
+                if (result != null)
+                {
+                    this.ListAudioFiles.Clear();
+                    foreach(var item in result)
+                    {
+                        this.ListAudioFiles.Add(item.FullPath);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private async void ButtonClicked(object sender, EventArgs e)
+        {
+            if(this.ListAudioFiles.Any())
+            {
+                var modalPage = new Page1();
+                modalPage.ListAudioFiles = this.ListAudioFiles;
+                modalPage.numberOfDiff = this.NumberOfDiff;
+                modalPage.resetCorrectPasses = this.CheckPoprawnosc.IsChecked;
+                modalPage.numberOfLive = this.NumberOfLive;
+                modalPage.numberOfMaxCorrectPasses = this.NumberOfMaxCorrectPasses;
+                modalPage.timeToThink = this.TimeToThink;
+                await PAUM.App.Current.MainPage.Navigation.PushModalAsync(modalPage);
+                modalPage.RoundStart();
+            }
+            else
+            {
+                this.ButtonClickedAudio(null, null);
+            }
+        }
+
+        private async void ButtonHowToPlayClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Jak grać?",
+                "W ekranie ustawień można dobrać wiele parametrów aby ułatwić rozgrywkę lub ją utrudnić." +
+                "Rozgrywka jest podzielona na rundy, każda runda rozpoczyna się puszczeniem dwóch dźwięków a potem następuje czas na zastanowienie." +
+                "Po odpowiedniej ilości poprawnych odpowiedzi gracz awansuje na wyższy poziom trudności." +
+                 "W grze występują 4 typy wskaźników:" +
+                "\n1. Swipe w lewo: puszczone dźwięki się róźnią" +
+                "\n2. Swipe w prawo: puszczone dźwięki są takie same" +
+                "\n3. Swipe w górę: powrót do ekranu ustawień" +
+                "\n4. Swipe w dół: powtarza dwa ostatnie dźwięki", "OK");
+        }
+    }
+}
---
+++ PAUM/PAUM/Views/Page1.xaml.cs
@@ -0,0 +1,287 @@
+﻿using AVFoundation;
+using Foundation;
+using Plugin.SimpleAudioPlayer;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Timers;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PAUM.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        int index0 { get; set; } = 0;
+        int index1 { get; set; } = 1;
+        public int numberOfLive { get; set; } = 3;
+        public int numberOfMaxCorrectPasses { get; set; } = 5;
+        public bool resetCorrectPasses { get; set; }
+        public int interval { get; set; } = 5;
+        public int numberOfDiff { get; set; } = 3;
+        public int timeToThink { get; set; } = 7;
+        int diff { get; set; } = 1;
+        int numberOfCorrectPasses { get; set; } = 1;
+        bool isRound { get; set; }
+        bool isHandled { get; set; }
+        public ObservableCollection<string> ListAudioFiles { get; set; } = new ObservableCollection<string>();
+        Random rnd;
+        Timer timer;
+        ISimpleAudioPlayer player = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.Current;
+
+        int MinSeconds
+        {
+            get
+            {
+                switch (this.diff)
+                {
+                    case 1: return 0;
+                    case 2: return 2;
+                    case 3: return 4;
+                }
+
+                return 0;
+            }
+        }
+
+        int MinSecondsToThink
+        {
+            get
+            {
+                switch (this.diff)
+                {
+                    case 1: return 0;
+                    case 2: return 1;
+                    case 3: return 2;
+                }
+
+                return 0;
+            }
+        }
+
+        public Page1()
+        {
+            this.rnd = new Random();
+            InitializeComponent();
+        }
+
+        public async void RoundStart()
+        {
+            try
+            {
+                this.isRound = true;
+                if(this.numberOfCorrectPasses == this.numberOfMaxCorrectPasses)
+                {
+                    this.numberOfMaxCorrectPasses = 0;
+                    if (this.diff < this.numberOfDiff)
+                    {
+                        await TextToSpeech.SpeakAsync("New difficulty!");
+                        this.diff++;
+                    }
+                    else if(this.diff == this.numberOfDiff)
+                    {
+                        await TextToSpeech.SpeakAsync("You won!");
+                        var modalPage = new MainPage();
+                        modalPage.ListAudioFiles = this.ListAudioFiles;
+                        modalPage.NumberOfDiff = this.numberOfDiff;
+                        modalPage.NumberOfLive = this.numberOfLive;
+                        modalPage.NumberOfMaxCorrectPasses = this.numberOfMaxCorrectPasses;
+                        modalPage.TimeToThink = this.timeToThink;
+                        await PAUM.App.Current.MainPage.Navigation.PushModalAsync(modalPage);
+                        return;
+                    }
+                }
+
+                if (this.numberOfLive == 0)
+                {
+                    await TextToSpeech.SpeakAsync("You lost!");
+                    Vibration.Vibrate();
+                    var modalPage = new MainPage();
+                    modalPage.ListAudioFiles = this.ListAudioFiles;
+                    modalPage.NumberOfDiff = this.numberOfDiff;
+                    modalPage.NumberOfLive = this.numberOfLive;
+                    modalPage.NumberOfMaxCorrectPasses = this.numberOfMaxCorrectPasses;
+                    modalPage.TimeToThink = this.timeToThink;
+                    await PAUM.App.Current.MainPage.Navigation.PushModalAsync(modalPage);
+                    return;
+                }
+
+                await Task.Delay(2000);
+                this.PlayFirstSound();
+                await Task.Delay((this.interval - this.MinSeconds) * 1000);
+                this.PlaySecondSound();
+
+                this.timer = new System.Timers.Timer((this.timeToThink - this.MinSecondsToThink) * 1000);
+                this.timer.Elapsed += OnTimedEvent;
+                this.timer.Start();
+                await Task.Delay(3000);
+                this.isRound = false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        async void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            if(this.isHandled)
+            {
+                this.isHandled = true;
+                this.timer.Stop();
+                await TextToSpeech.SpeakAsync("No time!");
+                Vibration.Vibrate();
+                this.numberOfLive--;
+                if (this.resetCorrectPasses) this.numberOfMaxCorrectPasses = 0;
+                this.RoundStart();
+                this.isHandled = false;
+            }
+        }
+
+        async void PlayFirstSound()
+        {
+            await TextToSpeech.SpeakAsync("First sound");
+            player.Load(this.ListAudioFiles[this.RandomNumber()]);
+            await Task.Delay(1000);
+            player.Play();
+        }
+
+        async void PlayFirstSound(int Index)
+        {
+            await TextToSpeech.SpeakAsync("First sound");
+            player.Load(this.ListAudioFiles[Index]);
+            await Task.Delay(1000);
+            player.Play();
+        }
+
+        async void PlaySecondSound()
+        {
+            await TextToSpeech.SpeakAsync("Second sound");
+            player.Load(this.ListAudioFiles[this.RandomNumber(true)]);
+            await Task.Delay(1000);
+            player.Play();
+        }
+
+        async void PlaySecondSound(int Index)
+        {
+            await TextToSpeech.SpeakAsync("Second sound");
+            player.Load(this.ListAudioFiles[Index]);
+            await Task.Delay(1000);
+            player.Play();
+        }
+
+        int RandomNumber(bool IsSecond = false)
+        {
+            int value = 0;
+
+            if(IsSecond)
+            {
+                if (rnd.Next(0, 100) < 33)
+                {
+                    this.index1 = this.index0;
+                    value = this.index1;
+                }
+                else
+                {
+                    this.index1 = rnd.Next(0, this.ListAudioFiles.Count());
+                    value = this.index1;
+                }
+            }
+            else
+            {
+                this.index0 = rnd.Next(0, this.ListAudioFiles.Count());
+                value = this.index0;
+            }
+            return value;
+        }
+
+        async private void OnSwipedLeft(object sender, SwipedEventArgs e)
+        {
+            if (!this.isRound && !this.isHandled)
+            {
+                this.isHandled = true;
+                this.timer.Stop();
+                if (this.index0 != this.index1)
+                {
+                    await TextToSpeech.SpeakAsync("Correct!");
+                    this.numberOfCorrectPasses++;
+                }
+                else
+                {
+                    await TextToSpeech.SpeakAsync("Wrong!");
+                    Vibration.Vibrate();
+                    this.numberOfLive--;
+                    if (this.resetCorrectPasses) this.numberOfMaxCorrectPasses = 0;
+                }
+                this.isHandled = false;
+                this.RoundStart();
+            }
+        }
+
+        async private void OnSwipedRight(object sender, SwipedEventArgs e)
+        {
+            if (!this.isRound && !this.isHandled)
+            {
+                this.isHandled = true;
+                this.timer.Stop();
+                if (this.index0 == this.index1)
+                {
+                    await TextToSpeech.SpeakAsync("Correct!");
+                    this.numberOfCorrectPasses++;
+                }
+                else
+                {
+                    await TextToSpeech.SpeakAsync("Wrong!");
+                    Vibration.Vibrate();
+                    this.numberOfLive--;
+                    if (this.resetCorrectPasses) this.numberOfMaxCorrectPasses = 0;
+                }
+                this.isHandled = false;
+                this.RoundStart();
+            }
+        }
+
+        async private void OnSwipedDown(object sender, SwipedEventArgs e)
+        {
+            if (!this.isHandled)
+            {
+                this.isHandled = true;
+                this.isRound = true;
+                this.PlayFirstSound(this.index0);
+                await Task.Delay((this.interval - this.MinSeconds) * 1000);
+                this.PlaySecondSound(this.index1);
+                await Task.Delay((this.interval - this.MinSeconds) * 1000);
+
+                this.timer = new System.Timers.Timer((this.timeToThink - this.MinSecondsToThink) * 1000);
+                this.timer.Elapsed += OnTimedEvent;
+                this.timer.Start();
+                await Task.Delay(3000);
+                this.isRound = false;
+                this.isHandled = false;
+            }
+        }
+
+        async private void OnSwipedUp(object sender, SwipedEventArgs e)
+        {
+            if (!this.isHandled)
+            {
+                this.isHandled = true;
+                var modalPage = new MainPage();
+                modalPage.ListAudioFiles = this.ListAudioFiles;
+                modalPage.NumberOfDiff = this.numberOfDiff;
+                modalPage.NumberOfLive = this.numberOfLive;
+                modalPage.NumberOfMaxCorrectPasses = this.numberOfMaxCorrectPasses;
+                modalPage.TimeToThink = this.timeToThink;
+                await PAUM.App.Current.MainPage.Navigation.PushModalAsync(modalPage);
+                this.isHandled = false;
+            }
+        }
+    }
+}
